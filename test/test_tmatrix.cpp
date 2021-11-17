35368158cc8be2a9148e967c9f3917f83185be56
yan4ikk <81555476+yan4ikk@users.noreply.github.com>
2021-11-17 12:33:17 +0300

    tests

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -9,7 +9,7 @@ TEST(TMatrix, can_create_matrix_with_positive_length)
 
 TEST(TMatrix, cant_create_too_large_matrix)
 {
-    ASSERT_ANY_THROW(TMatrix<int> m(TVector<int>::max_size - 1ull));
+    ASSERT_ANY_THROW(TMatrix<int>(static_cast<size_t>(TVector<int>::max_size + 1ull)));
 }
 
 TEST(TMatrix, throws_when_create_matrix_with_negative_length)
