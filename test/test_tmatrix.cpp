114f17c0d01ea40b09f2d631710ccc00ff8d5ae1
blast-sky <strogancev2020@gmail.com>
2021-11-20 12:59:56 +0300

    2

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -9,7 +9,7 @@ TEST(TMatrix, can_create_matrix_with_positive_length)
 
 TEST(TMatrix, cant_create_too_large_matrix)
 {
-    ASSERT_ANY_THROW(TMatrix<int>(static_cast<size_t>(std::sqrt(TVector<int>::max_size)) + 1ull));
+    ASSERT_ANY_THROW(TMatrix<int>(static_cast<size_t>(std::sqrt(TVector<int>::max_size) + 1ull)));
 }
 
 //TEST(TMatrix, create_max_size_matrix)
