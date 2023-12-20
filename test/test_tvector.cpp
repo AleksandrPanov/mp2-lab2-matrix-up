040202549ce0f81826c5256b28e3840969dd7971
m1likus <m1likus@yandex.ru>
2023-12-20 13:36:17 +0300

    modified

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -229,3 +229,14 @@ TEST(TVector, cant_multiply_vectors_with_not_equal_size)
     TVector <int> v(5), v1(6);
     ASSERT_ANY_THROW(v1 * v);
 }
+TEST(Tvector, can_create_huge_vector)
+{
+    const size_t size = 200ull * 100ull * 100ull * 100ull;
+    const size_t startIndex = size - 10ull;
+    TVector<double> v1(size, startIndex);
+    ASSERT_NO_THROW(v1.setElement(size - 1ull, 1.0));
+
+    EXPECT_EQ(size, v1.getSize());
+    EXPECT_EQ(1.0, v1.getElement(size - 1ull));
+
+}
