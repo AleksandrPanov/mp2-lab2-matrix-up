04ea4c2a17251da25e6871fbc627f5fdd2bcc9f8
Aleksey <yuhayo@mail.ru>
2024-02-19 18:48:03 +0300

    fixing

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -281,3 +281,18 @@ TEST(TVector, cant_multiply_vectors_with_not_equal_size)
     TVector <int> test2(4);
     ASSERT_ANY_THROW(test1 * test2);
 }
+
+TEST(Tvector, can_create_huge_vector)
+{
+    const size_t size = 200ull * 100ull * 100ull * 100ull;
+    const size_t startIndex = size - 10ull;
+    TVector<double> v1(size, startIndex);
+    v1.setElement(size - 1ull, 1.0);
+
+    ASSERT_NO_THROW(v1.setElement(size - 1ull, 1.0));
+    ASSERT_NO_THROW(v1[size - 1ull] = 1.0);
+
+    EXPECT_EQ(size, v1.getSize());
+    EXPECT_EQ(1.0, v1.getElement(size - 1ull));
+
+}
